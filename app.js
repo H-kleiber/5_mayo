282f4063786d715d5df5cf9c46a82ba7a9139bf8
H-kleiber <limnuev25@gmail.com>
2023-05-05 21:37:52 -0500

    objet

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function multiplos(numero){
         console.log(i+" "+"fizz");
        }else if(( i % 5)==0){
         console.log(i+" "+"buzz")
-       }else if (( i % 5)==0 && (i % 3)==0){
+       }else if (( i % 3)==0 && (i % 5)==0){
         console.log(i+" "+"fizz buzz");
        }
     }

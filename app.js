3ea67f1f7518e42b98f098067ca2ee9f03d4ab97
H-kleiber <limnuev25@gmail.com>
2023-05-05 21:47:53 -0500

    objet

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 // Crear un objeto literal. Con minimo 5 propiedades. Que contengan al menos 2 funciones. 
 // Y llamar las funcioes en consola , utilizando las demas propiedades del objeto.
-
 // let perro={
 //     name:"castor",
 //     raza:"Chihuahua",
 //     edad:1,
 //     color:"negro",
 //     size:"pequeño",
-//     Dato:function(){`el nombre de perro es ${this.name} es de raza ${this.raza}`},
-//     carasteristicas: function(){`el perro es de color ${this.color} y es de tamaño ${this.size}`}
+//     Dato:function(){return`el nombre de perro es ${this.name} es de raza ${this.raza}`},
+//     carasteristicas: function(){ return`el perro es de color ${this.color} y es de tamaño ${this.size}`}
 // }
+// console.log(perro);
+// console.log(perro.Dato());
     
 
 // Vamos a crear un algoritmo. Que lista del 1 al 30. Si el numero es multiplo de 3, 

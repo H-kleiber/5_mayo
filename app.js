8e5bcfdd960af05cbf3f53bfce6b2a9c023a1b71
H-kleiber <limnuev25@gmail.com>
2023-05-19 17:23:44 -0500

    eje

--- app.js
+++ app.js
@@ -473,3 +473,173 @@
 //     next=iterador.next();
     
 //  }
+
+
+// 11. Solicitar al usuario una calificación (entre 1 y 10). Luego se debe comprobar que el
+// número efectivamente esté en ese rango, si no lo está imprima un mensaje de
+// error. Si lo está, imprima “reprobado” si la calificación es inferior a 6, “regular” si
+// está entre 6 y 8, “bien” si es 9, y por último, “excelente” si es 10.
+
+
+// function calificacion(){
+//     let nota=prompt("ingrese la calificadion");
+//     nota=Number(nota);
+//     if (nota>=1 && nota<=10){
+//         if(nota<6){
+//             alert("Reprobado")
+//         }if(nota>=6 && nota<=8){
+//             alert("Regular")
+//         }if(nota===9){
+//             alert("Bien")
+//         }if(nota===10){
+//             alert("excelente")
+//         }
+//     }
+//     else{
+//         alert(" ERROR ; ingrese un rango de nota entra 1 y 10");
+//     }
+// }
+//  calificacion();
+// 12. Escribe un programa que responda a un usuario que quiere comprar un helado en
+// una conocida marca de comida rápida, cuánto le costará en función del topping
+// que elija.
+// • El helado sin topping cuesta 50 MXN.
+// • El topping de oreo cuesta 10 MXN.
+// • El topping de KitKat cuesta 15 MXN.
+// • El topping de brownie cuesta 20 MXN.
+// En caso de no disponer del topping solicitado por el usuario, el programa le
+// indicará “no tenemos este topping, lo sentimos.” y a continuación le informará el
+// precio del helado sin ningún topping.
+
+// function herlado(){
+//  const helados=[{name:"oreo",
+//                  precio:10},
+//                 {name:"kitkat",
+//                   precio:15}, 
+//                 {name:"brownie",
+//                  precio:20},
+//                 {name:"sin topping",
+//                  precio:50
+//                 }];
+
+//                  let entrada=prompt("eliga un helado ");
+//                  entrada=entrada.toLowerCase();
+//                  let array=[];
+
+//                 if (entrada!== ' '){
+//                     for (i=0;i<helados.length; i++){
+//                         if ((helados[i].name)===entrada){
+//                             array.push(helados[i].name)
+//                             console.log(array[0])
+//                             console.log(helados[i].precio+ "MXN ")
+//                             break;
+//                         }
+//                     }
+//                  }
+//                  }
+    
+//     herlado()
+
+
+// 13. Un conocido portal de educación en tecnología está ofreciendo programas para
+// aprender a desarrollar aplicaciones. Escribe un programa que le indique a la
+// persona interesada cuánto deberá pagar mensualmente de acuerdo a la opción
+// elegida.
+// El programa educativo contempla 3 diferentes niveles, cada uno con su costo
+// mensual:
+// • Course: $4999 MXN
+// • Carrera $3999 MXN
+// • Master: $2999 MXN
+// Adicionalmente preguntar si cuenta con alguna beca y aplicar el descuento
+// correspondiente al precio final.
+// • Beca Facebook: 20% de descuento.
+// • Beca Google: 15% de descuento.
+// • Beca Jesua: 50% de descuento.
+// Finalmente, además del precio mensual con descuento, indicar al usuario cuánto
+// gastaría en total por el curso elegido, tomando en cuenta las siguientes
+// duraciones:
+// • Course: 2 meses
+// • Carrera 6 meses
+// • Master: 12 meses
+
+
+
+
+
+
+// 14. Realizar un programa que ayude a calcular el total a pagar de acuerdo a la
+// distancia recorrida por un vehículo con cargo extra por los litros consumidos,
+// tomando en consideración lo siguiente:
+// Si el vehículo es “coche”, el precio kilometro ha de ser 0.20, si es “moto” ha de ser
+// 0.10 y si es “autobús” 0.5.
+// Si los litros consumidos están entre 0 y 100 se ha de añadir 5 al costo total, si es
+// mayor la cantidad de litros consumidos se ha de añadir 10 al total.
+// Considere qué: total a pagar = (precio kilometro x kms recorridos) + extra por litros
+// consumidos.
+
+
+// const btn = document.getElementById('btnCall');
+
+// const callConsole = () => {
+// const arrayNames = ['Diego', 'Jose', 'Pedro'];
+
+//   }
+// btn.addEventListener('click', callConsole);
+
+
+// const add=document.querySelector("section");
+// const ulList=document.createElement("ul"),list=document.createElement("li"),
+// list2=document.createElement("li");
+// contenido=document.createTextNode("secc");
+// contenido2=document.createTextNode("secc2");
+
+// list.appendChild(contenido)
+// list2.appendChild(contenido2)
+
+// add.appendChild(ulList);
+// ulList.appendChild(list)
+// ulList.appendChild(list2)
+
+// add.setAttribute("alt","animal");
+// add.classList.add("seccion")
+
+
+// console.log(add);
+
+// const circe=document.createElement("ul");
+// const arrglo=[1,2,3,4,5,6,7,8,9,10,11,12,13,141,15,16,17,18,19,20];
+// const fragmento=document.createDocumentFragment();
+
+// arrglo.forEach(el =>{
+//  const li=document.createElement("li");
+//  li.textContent=el;
+//  fragmento.appendChild(li);
+// });
+
+// circe.appendChild(fragmento),
+// document.body.appendChild(circe);
+
+
+const template=document.getElementById("template").content;
+ const fragmen=document.createDocumentFragment();
+ const card=document.querySelector(".cards")
+ console.log(card)
+
+ const array=[{name:"oreo",
+                  precio:"10"},
+                  {name:"kitkat",
+                   precio:"15"}, 
+                  {name:"brownie",
+                   precio:"20"},
+                  {name:"sin topping",
+                  precio:"50"
+                  }];
+array.forEach((el) =>{
+template.querySelector("img").setAttribute("alt",el.name);
+template.querySelector("figcaption").setAttribute("src",el.precio);
+
+let clone=document.importNode(template,true);
+fragmen.appendChild(clone);
+console.log(fragmen)
+});
+card.appendChild(fragmen)
